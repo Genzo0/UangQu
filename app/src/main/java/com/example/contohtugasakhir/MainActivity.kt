be6638c15bc4d036ca8b272a5172d68bfeb296b8
inlandsche <qolandara@gmail.com>
2022-12-07 10:51:29 +0700

    finishing program

--- app/src/main/java/com/example/contohtugasakhir/MainActivity.kt
+++ app/src/main/java/com/example/contohtugasakhir/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -40,7 +41,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var db : AppDatabase
     private lateinit var deletedTransaction : Transaction
     private lateinit var coordinator : CoordinatorLayout
-    private lateinit var queryEditText : TextInputEditText
+    private lateinit var queryEditText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         coordinator = findViewById(R.id.coordinator)
 
         transactions = arrayListOf()
+
 
         transactionAdapter = TransactionAdapter(transactions)
         linearLayoutManager = LinearLayoutManager(this)
